2914b2f499c961eec6be20b4cdeac4c32826ea0d
Murilo Antas Pereira <murilo.pereira@localiza.com>
2024-01-04 15:56:58 -0300

    change telescope path format

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,12 @@
+require('telescope').setup{
+    defaults = {
+        path_display = function(opts, path)
+            local tail = require('telescope.utils').path_tail(path)
+            return string.format('%s (%s)', tail, path)
+        end,
+    }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
